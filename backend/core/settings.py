ce443a63b9ed30e794985b26a61f9bca07d334f7
jahanzaib504 <jahanzaibawan2008@gmail.com>
2026-08-21 01:25:57 +0500

    Remove aws key from settings.py

--- backend/core/settings.py
+++ backend/core/settings.py
@@ -54,8 +54,7 @@ STORAGES = {
         "BACKEND": "django.contrib.staticfiles.storage.StaticFilesStorage",
     },
 }
-AWS_ACCESS_KEY_ID = config("AWS_ACCESS_KEY_ID")
-AWS_SECRET_ACCESS_KEY = config("AWS_SECRET_ACCESS_KEY")
+
 AWS_STORAGE_BUCKET_NAME = config("AWS_STORAGE_BUCKET_NAME")
 AWS_S3_REGION_NAME = config("AWS_S3_REGION_NAME")
 
